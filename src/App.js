f76bab9b3cc5f6278d569dd1f5786cfdf9aea09f
John Bohannon <imjohnbo@github.com>
2019-07-05 20:49:25 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  Box,
+  Button,
+  Heading,
+  Details,
+  Text
+} from '@primer/components';
 import './App.css';
+import routes from './routes.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  getList() {
+    // return Object.keys(routes)
+    //   .forEach(route => routes[route]
+    //     .map(endpoint => <p>{endpoint}</p>));
+    return routes;
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <Heading p={3}>API Endpoints</Heading>
+          {console.log(routes)}
+          <Details p={3}>
+            <h3 as="summary" p={3}>route.header</h3>
+            <p>This will show and hide</p>
+          </Details>
+      </div>
+    );
+  }
 }
 
 export default App;
